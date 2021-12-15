507b847948d50dadc3bff10e726c246b4c9ddd68
Santilp92 <salp7811@hotmail.com>
2021-12-14 22:09:00 -0500

    función SegundoNumeroMayor

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert ("saludos de Platzi")
+function segundoNumeroMayor (numeros){
+    let primero = numeros [0]
+    let segundo = 0;
+    for (let i=0; i < numeros.length; i++ ){
+        if (numeros [i] > primero){
+            segundo = primero;
+            primero = numeros [i]
+        }
+        if (numeros [i]> segundo && numeros[i]< primero){
+            segundo = numeros [0];
+        }
+    }
+    return segundo
+}
+let nums =[8,4,6,10,9,11]
+console.log(segundoNumeroMayor (nums))
